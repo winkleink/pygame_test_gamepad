8447c1d7a7e22e1e87b5da158fc28d234ec80b4d
winkleink <winkleink@gmail.com>
2016-03-29 17:46:22 +0100

    Creat pygame_test_gamepad.py
    Initial code for gamepad tester

---
+++ pygame_test_gamepad.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import os, sys, pygame 
+from pygame import locals
+import time
+
+# Define the colors we will use in RGB format
+black = [ 0, 0, 0]
+white = [255,255,255]
+red = [255, 0, 0]
+
+# set the delay between steps
+stepDelay = 0.002
+
+#os.environ["SDL_VIDEODRIVER"] = "dummy"
+pygame.init()
+
+# Set the height and width of the screen
+size=[800,600]
+screen=pygame.display.set_mode(size)
+# Fill the screen White
+screen.fill(white)
+# Put something in the application Bar
+pygame.display.set_caption("Testing key presses")
+
+pygame.draw.rect(screen,black,(20,20,276,276),0)
+
+pygame.display.flip()
+
+pygame.joystick.init() # main joystick device system
+
+deadZone = 0.6 # make a wide deadzone
+
+done = True
+
+try:
+    j = pygame.joystick.Joystick(0) # create a joystick instance
+    j.init() # init instance
+    print 'Enabled joystick: ' + j.get_name()
+except pygame.error:
+    print 'no joystick found.'
+   
+while done:
+    for e in pygame.event.get(): # iterate over event stack
+        if e.type == pygame.QUIT:
+            done = False
+        
+        if e.type == pygame.locals.JOYAXISMOTION: # Read Analog Joystick Axis
+            x1 , y1 = j.get_axis(0), j.get_axis(1) # Left Stick
+
+            print x1
+            print y1
+            pygame.draw.rect(screen,black,(19,19,276,276),0)
+            pygame.draw.rect(screen,white,(148+(x1*128),148+(y1*128),20,20),0)
+            pygame.display.flip()
+
+        
+        if e.type == pygame.locals.JOYBUTTONDOWN: # Read the buttons
+            print ("button down"+str(e.button))
+            for i in range(0,4):
+                if e.button == i:
+                    pygame.draw.rect(screen,red,(300+(i*30),20,20,20),0)
+                    pygame.display.flip()
+            
+
+        if e.type == pygame.locals.JOYBUTTONUP: # Read the buttons
+            print ("button up"+str(e.button))
+            for i in range(0,4):
+                if e.button == i:
+                    pygame.draw.rect(screen,white,(300+(i*30),20,20,20),0)
+                    pygame.display.flip()
